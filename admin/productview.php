20272f790e0aef29da2396d60e919dcc7524c722
ESTON-kiam <engestonbrandonkiama@gmail.com>
2025-03-20 15:35:24 +0300

    update

--- admin/productview.php
+++ admin/productview.php
@@ -40,33 +40,7 @@ $result = $conn->query($query);
     <link href="assets/img/cart.jpg" rel="apple-touch-icon">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/adminviewproduct.css">
-    <style>
-        .price-original {
-            text-decoration: line-through;
-            color: #777;
-            margin-right: 8px;
-        }
-        .price-discounted {
-            color: #e63946;
-            font-weight: bold;
-        }
-        .discount-badge {
-            position: absolute;
-            top: 0;
-            right: 0;
-            background-color: #e63946;
-            color: white;
-            padding: 2px 6px;
-            font-size: 0.8em;
-            font-weight: bold;
-            border-radius: 3px;
-        }
-        .product-image-container {
-            position: relative;
-            width: 100px;
-            height: 100px;
-        }
-    </style>
+   
 </head>
 <body>
 <?php include('include/sidebar.php'); ?>
